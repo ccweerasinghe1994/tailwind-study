39bfafb4c17972a8e47719bf5c00da46369ea277
wchamara <chamara.weerasinghe@my.sliit.lk>
2022-05-21 08:34:31 +0530

    adding discover section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,18 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ['./dist/*.{html,js}'],
   theme: {
-
     extend: {
-      fontFamily:{
-        'comf':['Comfortaa', 'cursive'],
-        'lora':['Lora','serif'],
-        'nun':['Nunito','sans-serif'],
-        'libre':['Libre Baskerville','serif'],
-        'monte':['Montserrat','serif'],
-        sans: ['Nunito',...defaultTheme.fontFamily.sans],
-      }
+      fontFamily: {
+        comf: ['Comfortaa', 'cursive'],
+        lora: ['Lora', 'serif'],
+        nun: ['Nunito', 'sans-serif'],
+        libre: ['Libre Baskerville', 'serif'],
+        monte: ['Montserrat', 'serif'],
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
-}
+};
